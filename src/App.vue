7b0781b0f8baef9c6a5ddf2069482a863bf646e6
maksymilian-lakomy <zommer128@gmail.com>
2020-02-07 18:35:48 +0100

    Created header

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+	<div id="app">
+        <Header />
+		<router-view />
+	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+	components: {
+        Header,
+		HelloWorld
+	}
 })
 export default class App extends Vue {}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css?family=Libre+Baskerville:400,700|Raleway:400,500,700,900&display=swap')
+
+body
+    margin: unset
+    min-height: 100vh
+
+#app
+	font-family: 'Libre Baskerville', serif
+	-webkit-font-smoothing: antialiased
+	-moz-osx-font-smoothing: grayscale
+	background-color: black
+	color: white
+	min-height: inherit
+    font-size: 16px
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+	<header>
+		<nav class="nav--left">
+			<button>B</button>
+		</nav>
+		<nav class="nav--center">
+			<button>B</button>
+		</nav>
+		<nav class="nav--right">
+			<button>B</button>
+		</nav>
+	</header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class App extends Vue {
+}
+</script>
+
+<style lang="sass" scoped>
+header
+    display: grid
+    grid-template-columns: repeat(3, calc(100% / 3))
+
+    nav
+        height: 1.5rem
+        margin: 2.5rem 1.25rem
+
+        button
+            position: relative
+            color: inherit
+            border: unset
+            background-color: unset
+            outline: unset
+            margin: unset
+            padding: unset
+            height: 1.5rem
+            width: 1.5rem
+
+            &::before
+                cursor: pointer
+                content: ""
+                position: absolute
+                width: 200%
+                height: 200%
+                top: -50%
+                left: -50%
+
+    .nav--left
+        text-align: left
+
+    .nav--center
+        text-align: center
+
+    .nav--right
+        text-align: right
+        
+
+</style>
